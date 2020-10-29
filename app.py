b232bc167dc71d198755b648347266e303eba17c
u1234x1234 <u1234x1234@gmail.com>
2020-10-29 21:28:42 +0300

    up

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
 import fire
+from uxils.video.io import read_video_av
+
+
+class VideoLandmarkExtractor:
+    def __init__(self, max_size=None):
+        from uxils.image.face.alignment import DLibFaceLandmarksExtractor
+
+        self.ex = DLibFaceLandmarksExtractor(max_size=max_size)
+
+    def extract(self, path):
+        frames = read_video_av(path)
+        print(len(frames))
+        return [1] * len(frames)
+        # return self.ex.extract_landmarks(frames)
+
+
+def extract_landmarks():
+    from uxils.file_system import glob_videos
+    from uxils.multiprocessing_ext.map import map_class_pickle_db
+
+    paths = glob_videos("data/2020-1/val")
+
+    map_class_pickle_db(
+        (VideoLandmarkExtractor, "extract", dict(max_size=400)),
+        paths,
+        "landmarks_val.pkll",
+        batch=False,
+        n_workers=40
+    )
+
+
+if __name__ == "__main__":
+    fire.Fire()
--- exp.py
+++ exp.py
@@ -1,12 +1,42 @@
-from uxils.video.io import read_video_av, read_video_cv2
-from uxils.timer import Timer
+import cv2
+import numpy as np
+import multiprocessing
 from uxils.file_system import glob_videos
+from uxils.image.draw import draw_points
 from uxils.image.vis import show_image
+from uxils.profiling import Profiler
+from uxils.timer import Timer
+from uxils.video.io import read_video_av
+from uxils.db.pickle_db import read_pkll_generator
+from uxils.video.vis import show_frames
 
 
-for path in glob_videos("data/2020-1/val"):
-    print(path)
+in_filename = 'data/2020-1/val/48960-4-085-w-67-036-dis-sad-dis.mp4'
+paths = glob_videos("data/2020-1/val")
+
+
+def func(path):
+    frames = read_video_av(path)
+    return len(frames)
+
+
+with multiprocessing.Pool(40) as pool:
+    pool.map(func, paths)
+
+qwe
+
+# landmarks = read_pkll_generator("landmarks_val.pkll")
+# print(len(list(landmarks)))
+
+for path, predictions in landmarks:
+
     with Timer():
         frames = read_video_av(path)
-    for frame in frames:
-        show_image(frame)
+
+    print(len(frames), len(predictions))
+    for l_per_i, image in zip(predictions, frames):
+        for l_per_box in l_per_i:
+            draw_points(image, l_per_box)
+
+    show_frames(frames)
+        # show_image(image, wait=False)
