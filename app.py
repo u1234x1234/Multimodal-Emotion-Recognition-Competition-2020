62c7e30332a63e3448fd6d5b0b8c6dfb05278f4e
u1234x1234 <u1234x1234@gmail.com>
2020-11-06 12:00:43 +0300

    up

--- app.py
+++ app.py
@@ -1,32 +1,48 @@
+import os
+
 import fire
-from uxils.video.io import read_video_av
+import numpy as np
+from uxils.file_system import glob_videos
+from uxils.image.face.alignment import crop_face_from_landmarks
+from uxils.timer import Timer
+from uxils.video.io import read_video_av_generator, write_frames_to_file
 
 
 class VideoLandmarkExtractor:
-    def __init__(self, max_size=None):
+    def __init__(self, out_dir, max_size=None):
         from uxils.image.face.alignment import DLibFaceLandmarksExtractor
 
         self.ex = DLibFaceLandmarksExtractor(max_size=max_size)
+        self.out_dir = out_dir
 
     def extract(self, path):
-        frames = read_video_av(path)
-        print(len(frames))
-        return [1] * len(frames)
-        # return self.ex.extract_landmarks(frames)
+        try:
+            face_images = []
+            for frame in read_video_av_generator(path, threads=False, sparse=10):
+                landmarks = self.ex.extract_landmarks(frame)
+
+                if landmarks:
+                    face_image, _ = crop_face_from_landmarks(frame, landmarks[0])
+                    face_images.append(face_image)
+
+            write_frames_to_file(
+                f"{self.out_dir}/{os.path.basename(path)}", face_images
+            )
+        except Exception as e:
+            print(path, e)
 
 
 def extract_landmarks():
-    from uxils.file_system import glob_videos
-    from uxils.multiprocessing_ext.map import map_class_pickle_db
+    from uxils.multiprocessing_ext.map import map_class
 
-    paths = glob_videos("data/2020-1/val")
+    paths = glob_videos("data/2020-1/train")
+    np.random.shuffle(paths)
+    print(len(paths))
 
-    map_class_pickle_db(
-        (VideoLandmarkExtractor, "extract", dict(max_size=400)),
+    map_class(
+        (VideoLandmarkExtractor, "extract", dict(out_dir="face_images", max_size=400)),
         paths,
-        "landmarks_val.pkll",
-        batch=False,
-        n_workers=40
+        n_workers=40,
     )
 
 
--- exp.py
+++ exp.py
@@ -1,32 +1,29 @@
-import cv2
 import numpy as np
 import multiprocessing
 from uxils.file_system import glob_videos
 from uxils.image.draw import draw_points
-from uxils.image.vis import show_image
-from uxils.profiling import Profiler
+from uxils.image.vis import show_image, show_images_hstack
 from uxils.timer import Timer
-from uxils.video.io import read_video_av
+from uxils.video.io import read_video_av, read_video_cv2, read_video_av_generator, read_video_cv2_generator
 from uxils.db.pickle_db import read_pkll_generator
-from uxils.video.vis import show_frames
-
+# from uxils.video.vis import show_frames
+from app import VideoLandmarkExtractor
 
 in_filename = 'data/2020-1/val/48960-4-085-w-67-036-dis-sad-dis.mp4'
-paths = glob_videos("data/2020-1/val")
+# paths = glob_videos("data/2020-1/val")
+paths = glob_videos("face_images")
 
+ex = VideoLandmarkExtractor("face_images", max_size=400)
 
-def func(path):
-    frames = read_video_av(path)
-    return len(frames)
+for path in paths:
+    with Timer():
+        r = read_video_av(path)
 
+    show_images_hstack(r)
 
-with multiprocessing.Pool(40) as pool:
-    pool.map(func, paths)
-
-qwe
 
 # landmarks = read_pkll_generator("landmarks_val.pkll")
-# print(len(list(landmarks)))
+print(len(list(landmarks)))
 
 for path, predictions in landmarks:
 
